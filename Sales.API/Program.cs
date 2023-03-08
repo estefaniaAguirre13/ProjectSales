7d3f376bf35f957fa1498280dde6e296ea9f2c60
estefania.aguirre@ingeneo.com.co <estefania.aguirre@ingeneo.com.co>
2023-03-08 09:10:04 -0500

    External API

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Sales.API.Data;
+using Sales.API.Services;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -13,6 +14,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 builder.Services.AddTransient<SeedDb>();
+builder.Services.AddScoped<IApiService, ApiService>();
+
 
 var app = builder.Build();
 
