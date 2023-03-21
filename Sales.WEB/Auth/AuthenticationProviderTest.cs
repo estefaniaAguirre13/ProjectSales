e75c79363fcdefa87a474515cdeb33890538d3f8
estefania.aguirre@ingeneo.com.co <estefania.aguirre@ingeneo.com.co>
2023-03-21 09:40:46 -0500

    Security hardcoded on WEB

---
+++ Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var tefaUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Estefa"),
+                new Claim("LastName", "Aguirre"),
+                new Claim(ClaimTypes.Name, "tefa@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            },
+            authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(tefaUser)));
+        }
+    }
+}
+
--- Sales.WEB/Program.cs
+++ Sales.WEB/Program.cs
@@ -1,7 +1,9 @@
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sales.WEB;
+using Sales.WEB.Auth;
 using Sales.WEB.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,8 +11,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7138/") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7138/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
+
 
 await builder.Build().RunAsync();
