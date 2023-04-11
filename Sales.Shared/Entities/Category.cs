88a3b26880fa36031e9e1b64aa2427e71dfb5d4d
estefania.aguirre@ingeneo.com.co <estefania.aguirre@ingeneo.com.co>
2023-04-11 09:50:35 -0500

    table and list of products

--- Sales.Shared/Entities/Category.cs
+++ Sales.Shared/Entities/Category.cs
@@ -17,5 +17,11 @@ namespace Sales.Shared.Entities
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caractéres")]
         public string Name { get; set; } = null!;
+
+        public ICollection<ProductCategory>? ProductCategories { get; set; }
+
+        [Display(Name = "Productos")]
+        public int ProductCategoriesNumber => ProductCategories == null ? 0 : ProductCategories.Count;
+
     }
 }
